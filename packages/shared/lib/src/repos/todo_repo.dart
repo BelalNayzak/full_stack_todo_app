0ec0eb728b75f9c720c12b881b07ff5091021450
BelalNayzak <belal.nayzak@gmail.com>
2025-10-29 21:26:04 +0300

    [shared] [feat] : added logout interfacing and editted todo, use repos

--- packages/shared/lib/src/repos/todo_repo.dart
+++ packages/shared/lib/src/repos/todo_repo.dart
@@ -2,7 +2,7 @@ import 'package:shared/shared.dart';
 
 abstract class TodoRepo {
   Future<TodoModel> createTodo(TodoModel todoModel);
-  Future<List<TodoModel>> getAllTodos();
+  Future<List<TodoModel>> getAllTodos(int userId);
   Future<TodoModel?> getTodo(int id);
   Future<TodoModel?> updateTodo(TodoModel updatedTodoModel);
   Future<TodoModel?> deleteTodo(int id);
--- packages/shared/lib/src/repos/user_repo.dart
+++ packages/shared/lib/src/repos/user_repo.dart
@@ -4,6 +4,7 @@ abstract class UserRepo {
   Future<UserModel> createUser(UserModel userModel);
   Future<List<UserModel>> getAllUsers();
   Future<UserModel?> getUserById(int id);
+  Future<UserModel?> getUserByToken(String token);
   Future<UserModel?> getUserByEmail(String email);
   Future<UserModel?> updateUser(UserModel updatedUserModel);
   Future<UserModel?> deleteUser(int id);
