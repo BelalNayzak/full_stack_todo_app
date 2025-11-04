278550a92ab8ac2f61cebd65132b8565c9317c73
BelalNayzak <belal.nayzak@gmail.com>
2025-11-04 17:07:11 +0200

    [backend] [tag] : v1.0.0

--- packages/backend/routes/index.dart
+++ packages/backend/routes/index.dart
@@ -2,7 +2,7 @@ import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
   return Response.json(
-    body: '''|   Made with ❤️ by Belal Ashraf   |   
-        Backend is running successfully 🎉   |''',
+    body:
+        '''|   Made with ❤️ by Belal Ashraf   |   Backend is running successfully 🎉   |''',
   );
 }
