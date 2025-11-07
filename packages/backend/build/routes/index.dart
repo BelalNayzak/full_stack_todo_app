03dbb02d2533440a71cf2fcdfe93a1590e8191f3
BelalNayzak <belal.nayzak@gmail.com>
2025-11-08 01:18:49 +0200

    [backend] [chore] : deply the last build

--- packages/backend/build/routes/index.dart
+++ packages/backend/build/routes/index.dart
@@ -1,8 +1,43 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'package:backend_dart_frog/backend.dart';
 
-Response onRequest(RequestContext context) {
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => _onGet(context),
+    HttpMethod.post => _onPost(context),
+    _ => Future.value(
+      Response(
+        statusCode: HttpStatus.methodNotAllowed,
+        body: 'Method not allowed',
+      ),
+    ),
+  };
+}
+
+Future<Response> _onGet(RequestContext context) async {
   return Response.json(
     body:
         '''|   Backend is running successfully 🎉   |   Made with ❤️ by Belal Ashraf   |''',
   );
+}
+
+Future<Response> _onPost(RequestContext context) async {
+  try {
+    final result = await context.read<Pool>().execute(
+      Sql.named('SELECT * FROM "uptime_robot_msg" ORDER BY "id" ASC'),
+    );
+
+    // data processing
+    final data = result.map((record) => record.toColumnMap()).toList();
+
+    return Response.json(
+      statusCode: HttpStatus.ok,
+      body: '${data.first['msg']}',
+    );
+  } catch (e) {
+    return Response.json(
+      statusCode: HttpStatus.badRequest,
+      body:
+          'Sorry, Please try again after in 1-2 minute(s). The api was in sleep-mode and it\'s waking up now...',
+    );
+  }
 }
